c4bb0b1764de16a37269de5e4c009240cf8e177a
Tuğhan Kızmaz <84676377+Tugiki@users.noreply.github.com>
2025-12-23 00:02:35 +0300

    Player Changer

--- Assets/Character/Scripts/PlayerChanger.cs
+++ Assets/Character/Scripts/PlayerChanger.cs
@@ -3,14 +3,20 @@ using UnityEngine;
 public class PlayerChanger : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+
+    [SerializeField] GameObject changer;
+    [SerializeField] GameObject _changed;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+
+            changer.SetActive(false);
+            _changed.SetActive(true);
+            
+            Debug.Log("Pressed R");
+        }
     }
 }
