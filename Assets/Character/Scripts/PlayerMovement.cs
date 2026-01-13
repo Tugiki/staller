539047a3480373ed931c57b8db11a168692a1e34
Tuğhan Kızmaz <tughankizmaz@windowslive.com>
2026-01-13 12:22:32 +0300

    Some adjustments for 2d

--- Assets/Character/Scripts/PlayerMovement.cs
+++ Assets/Character/Scripts/PlayerMovement.cs
@@ -1,33 +1,70 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float moveSpeed = 1f;
+    [Header("References")]
+    private CharacterController controller;
 
-    public Rigidbody rb;
-    public Animator animator; 
-    
-    
-    void Update()
+    [Header("Movement Settings")]
+    [SerializeField] private float walkSpeed = 1f;
+
+
+    [SerializeField] private float gravity = 9.81f;
+
+    private float verticalVelocity;
+
+    [Header("Input")]
+    private float moveInput;
+    private float turnInput;
+
+    private void InputManagement()
     {
-        float x = Input.GetAxisRaw("Horizontal");
-        float z = Input.GetAxisRaw("Vertical");
-        
-        animator.SetFloat("Horizontal", x);
-        animator.SetFloat("Vertical", z);
-        
+        moveInput = Input.GetAxis("Vertical");
+        turnInput = Input.GetAxis("Horizontal");
 
-        Vector3 move = transform.forward * z + transform.right * x;
-        transform.position += move * moveSpeed * Time.deltaTime;
 
-        animator.SetFloat("Speed", move.sqrMagnitude);
     }
 
-    void FixedUpdate()
+    private void GroundMovement()
     {
+        Vector3 move = new Vector3(turnInput, 0, moveInput);
 
+        move.y = VerticalForceCalculation();
+
+        move *= walkSpeed;
+
+        controller.Move(move * Time.deltaTime);
     }
+
+    private void Movement()
+    {
+        GroundMovement();
+    }
+
+    private float VerticalForceCalculation()
+    {
+        if (controller.isGrounded)
+        {
+            verticalVelocity = -1f; // Small negative value to keep the player grounded
+        }
+        else
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+        return verticalVelocity;
+    }
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        InputManagement();
+        Movement();
+    }
+
 
 }
